e4055ee2ecddc3facd633231f0b0f3d8d274339d
DJ <dj.leamen@ontariotechu.net>
2026-01-05 22:27:52 -0500

    repl

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 int main() {
   // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  cout << unitbuf;
+  cerr << unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
+  // Read: Display a prompt and wait for user input
+  while (true) {
+    cout << "$ ";
+    string command;
+    getline(cin, command);
 
-  std::string command;
-  std::getline(std::cin, command);
+    // Eval: Parse and execute the command
   
-  std::cout << command << ": command not found" << std::endl;
+    // Print: Display the output or error message
+    cout << command << ": command not found" << endl;
+
+    // Loop: Return to step 1 and wait for the next command
+  }
+  return 0;
 }
