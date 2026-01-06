c128ab803432a6841e5d79ff34d83c8591fc9e9d
DJ <dj.leamen@ontariotechu.net>
2026-01-05 22:56:50 -0500

    PATH

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <filesystem>
+#include <cstdlib>
 
 using namespace std;
+namespace fs = std::filesystem;
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -15,18 +19,44 @@ int main() {
     getline(cin, command);
 
     // Eval: Parse and execute the command
+
+    // exit
     if (command == "exit") {
       break;
     }
+    // echo
     else if (command.substr(0, 4) == "echo") {
       cout << command.substr(5) << endl;
     }
+    // type
     else if (command.substr(0, 5) == "type ") {
       string arg = command.substr(5);
       if (arg == "exit" || arg == "echo" || arg == "type") {
         cout << arg << " is a shell builtin" << endl;
       } else {
-        cout << arg << ": not found" << endl;
+        // Search for executable in PATH
+        const char* path_env = getenv("PATH");
+        if (path_env) {
+          string path_str(path_env);
+          stringstream ss(path_str);
+          string dir;
+          bool found = false;
+          
+          while (getline(ss, dir, ':')) {
+            fs::path full_path = fs::path(dir) / arg;
+            if (fs::exists(full_path) && (fs::status(full_path).permissions() & fs::perms::owner_exec) != fs::perms::none) {
+              cout << arg << " is " << full_path.string() << endl;
+              found = true;
+              break;
+            }
+          }
+          
+          if (!found) {
+            cout << arg << ": not found" << endl;
+          }
+        } else {
+          cout << arg << ": not found" << endl;
+        }
       }
     }
     // Print: Display the output or error message
