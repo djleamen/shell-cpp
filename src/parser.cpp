041668d770fe2d34fec93e82d3f989534b5b9fbb
DJ Leamen <dilara_leamen@icloud.com>
2026-05-03 19:48:42 -0400

    codecrafters submit [skip ci]

--- src/parser.cpp
+++ src/parser.cpp
@@ -4,143 +4,106 @@
  */
 #include "parser.h"
 
-#include <sstream>
 #include <cctype>
 
 using namespace std;
 
-PipelineInfo parsePipeline(const string& command) {
-  PipelineInfo pipeline;
-  pipeline.has_pipe = false;
+static pair<bool, vector<string>> splitByPipe(const string& command) {
+  vector<string> segments;
+  string current;
+  bool in_single = false, in_double = false, has_pipe = false;
 
-  vector<string> command_strings;
-  string current_cmd;
-  bool in_single_quotes = false;
-  bool in_double_quotes = false;
-
-  for (size_t i = 0; i < command.length(); ++i) {
+  for (size_t i = 0; i < command.size(); ++i) {
     char c = command[i];
-
-    if (c == '\\' && !in_single_quotes && i + 1 < command.length()) {
-      current_cmd += c;
-      current_cmd += command[++i];
-    }
-    else if (c == '\'' && !in_double_quotes) {
-      in_single_quotes = !in_single_quotes;
-      current_cmd += c;
-    }
-    else if (c == '"' && !in_single_quotes) {
-      in_double_quotes = !in_double_quotes;
-      current_cmd += c;
-    }
-    else if (c == '|' && !in_single_quotes && !in_double_quotes) {
-      if (!current_cmd.empty()) {
-        command_strings.push_back(current_cmd);
-        current_cmd.clear();
-      }
-      pipeline.has_pipe = true;
-    }
-    else {
-      current_cmd += c;
+    if (c == '\\' && !in_single && i + 1 < command.size()) {
+      current += c;
+      current += command[++i];
+    } else if (c == '\'' && !in_double) {
+      in_single = !in_single;
+      current += c;
+    } else if (c == '"' && !in_single) {
+      in_double = !in_double;
+      current += c;
+    } else if (c == '|' && !in_single && !in_double) {
+      if (!current.empty()) segments.push_back(current);
+      current.clear();
+      has_pipe = true;
+    } else {
+      current += c;
     }
   }
-  if (!current_cmd.empty()) {
-    command_strings.push_back(current_cmd);
-  }
+  if (!current.empty()) segments.push_back(current);
+  return {has_pipe, segments};
+}
 
-  for (const auto& cmd_str : command_strings) {
-    CommandInfo info;
-    info.has_redirect = false;
-    info.is_append = false;
-    info.has_error_redirect = false;
-    info.is_error_append = false;
+static vector<string> tokenise(const string& cmd_str) {
+  vector<string> tokens;
+  string current;
+  bool in_single = false, in_double = false;
 
-    vector<string> args;
-    string current_arg;
-    in_single_quotes = false;
-    in_double_quotes = false;
-
-    for (size_t i = 0; i < cmd_str.length(); ++i) {
-      char c = cmd_str[i];
-
-      if (c == '\\' && !in_single_quotes && !in_double_quotes) {
-        if (i + 1 < cmd_str.length()) {
-          ++i;
-          current_arg += cmd_str[i];
-        }
-        else {
-          current_arg += c;
-        }
-      }
-      else if (c == '\\' && in_double_quotes && !in_single_quotes) {
-        if (i + 1 < cmd_str.length()) {
-          char next = cmd_str[i + 1];
-          if (next == '"' || next == '\\') {
-            ++i;
-            current_arg += next;
-          } else {
-            current_arg += c;
-          }
-        } else {
-          current_arg += c;
-        }
-      }
-      else if (c == '\'' && !in_double_quotes) {
-        in_single_quotes = !in_single_quotes;
-      } else if (c == '"' && !in_single_quotes) {
-        in_double_quotes = !in_double_quotes;
-      } else if (isspace(c) && !in_single_quotes && !in_double_quotes) {
-        if (!current_arg.empty()) {
-          args.push_back(current_arg);
-          current_arg.clear();
-        }
+  for (size_t i = 0; i < cmd_str.size(); ++i) {
+    char c = cmd_str[i];
+    if (c == '\\' && !in_single && !in_double) {
+      if (i + 1 < cmd_str.size()) current += cmd_str[++i];
+      else                         current += c;
+    } else if (c == '\\' && in_double) {
+      if (i + 1 < cmd_str.size()) {
+        char next = cmd_str[i + 1];
+        if (next == '"' || next == '\\') { current += next; ++i; }
+        else                              current += c;
       } else {
-        current_arg += c;
+        current += c;
       }
+    } else if (c == '\'' && !in_double) {
+      in_single = !in_single;
+    } else if (c == '"' && !in_single) {
+      in_double = !in_double;
+    } else if (isspace(static_cast<unsigned char>(c)) && !in_single && !in_double) {
+      if (!current.empty()) { tokens.push_back(current); current.clear(); }
+    } else {
+      current += c;
     }
-    if (!current_arg.empty()) {
-      args.push_back(current_arg);
-    }
-
-    for (size_t i = 0; i < args.size(); ++i) {
-      if (args[i] == ">>" || args[i] == "1>>") {
-        info.has_redirect = true;
-        info.is_append = true;
-        if (i + 1 < args.size()) {
-          info.output_file = args[i + 1];
-          args.erase(args.begin() + i, args.begin() + i + 2);
-          --i;
-        }
-      } else if (args[i] == ">" || args[i] == "1>") {
-        info.has_redirect = true;
-        info.is_append = false;
-        if (i + 1 < args.size()) {
-          info.output_file = args[i + 1];
-          args.erase(args.begin() + i, args.begin() + i + 2);
-          --i;
-        }
-      } else if (args[i] == "2>>") {
-        info.has_error_redirect = true;
-        info.is_error_append = true;
-        if (i + 1 < args.size()) {
-          info.error_file = args[i + 1];
-          args.erase(args.begin() + i, args.begin() + i + 2);
-          --i;
-        }
-      } else if (args[i] == "2>") {
-        info.has_error_redirect = true;
-        info.is_error_append = false;
-        if (i + 1 < args.size()) {
-          info.error_file = args[i + 1];
-          args.erase(args.begin() + i, args.begin() + i + 2);
-          --i;
-        }
-      }
-    }
-
-    info.args = args;
-    pipeline.commands.push_back(info);
   }
+  if (!current.empty()) tokens.push_back(current);
+  return tokens;
+}
 
+static void extractRedirects(vector<string>& args, CommandInfo& info) {
+  for (size_t i = 0; i + 1 < args.size(); ++i) {
+    const string& tok = args[i];
+    if (tok == ">>" || tok == "1>>") {
+      info.has_redirect = true; info.is_append = true;
+      info.output_file  = args[i + 1];
+      args.erase(args.begin() + i, args.begin() + i + 2); --i;
+    } else if (tok == ">" || tok == "1>") {
+      info.has_redirect = true; info.is_append = false;
+      info.output_file  = args[i + 1];
+      args.erase(args.begin() + i, args.begin() + i + 2); --i;
+    } else if (tok == "2>>") {
+      info.has_error_redirect = true; info.is_error_append = true;
+      info.error_file = args[i + 1];
+      args.erase(args.begin() + i, args.begin() + i + 2); --i;
+    } else if (tok == "2>") {
+      info.has_error_redirect = true; info.is_error_append = false;
+      info.error_file = args[i + 1];
+      args.erase(args.begin() + i, args.begin() + i + 2); --i;
+    }
+  }
+}
+
+static CommandInfo parseCommand(const string& cmd_str) {
+  CommandInfo info{};
+  vector<string> args = tokenise(cmd_str);
+  extractRedirects(args, info);
+  info.args = move(args);
+  return info;
+}
+
+PipelineInfo parsePipeline(const string& command) {
+  auto [has_pipe, segments] = splitByPipe(command);
+  PipelineInfo pipeline;
+  pipeline.has_pipe = has_pipe;
+  for (const auto& seg : segments)
+    pipeline.commands.push_back(parseCommand(seg));
   return pipeline;
 }
